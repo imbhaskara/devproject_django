adb4a31a4d6b1738514f4a9eb2fe312bed534ea3
imbhaskara <imam.bhaskara@gmail.com>
2022-06-19 11:50:33 +0700

    First Migration Data Project, Review and Tags dan create Super Admin

--- devproject/projects/admin.py
+++ devproject/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# Register our model here
+from .models import Project, Review, Tag
+admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Tag)
 # Register your models here.
