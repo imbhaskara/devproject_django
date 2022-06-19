e2f64fdb9aff548d24dafd5d3cd7d7ddf875fb7d
imbhaskara <imam.bhaskara@gmail.com>
2022-06-19 09:30:50 +0700

    Rendering Templates & Read Data From List

--- devproject/projects/views.py
+++ devproject/projects/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': 1,
+        'title': 'Ecommerce Website',
+        'description': 'This is a ecommerce website',
+    },
+    {
+        'id': 2,
+        'title': 'Porfolio Website',
+        'description': 'This was a project where I built a portfolio website',
+    },
+    {
+        'id': 3,
+        'title': 'Social Network Website',
+        'description': 'Awesome open source social network website project I am still working on',
+    },
+]
+
 # Create your views here.
 def projects(request):
-    return HttpResponse("Here are our products")
+    number = 7
+    context = {'number':number, 'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return HttpResponse("You are looking at project number " +  str(pk))
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+             projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
