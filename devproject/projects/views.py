12e569b347e3fd4e46091a7f0b7052be23db8b2b
imbhaskara <imam.bhaskara@gmail.com>
2022-06-19 07:43:37 +0700

    First Learning How to Use View and Route

---
+++ devproject/projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def projects(request):
+    return HttpResponse("Here are our products")
+
+def project(request, pk):
+    return HttpResponse("You are looking at project number %s" % pk)
