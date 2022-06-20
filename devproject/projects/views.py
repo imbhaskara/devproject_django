73b2f7833c6a66f4a9eb1344d4f4a5174f5bd4b1
imbhaskara <imam.bhaskara@gmail.com>
2022-06-20 19:39:13 +0700

    Using Model Form and CRUD in Django

--- devproject/projects/views.py
+++ devproject/projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #import Model kita ke dalam views
 from .models import Project
-
+#import form yang sudah kita buat melalui forms.py
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -12,3 +13,32 @@ def projects(request):
 def project(request, pk):
     project = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project':project})
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form}
+    return render(request, 'projects/forms.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form}
+    return render(request, 'projects/forms.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'project':project}
+    return render(request, 'projects/delete-forms.html', context)
